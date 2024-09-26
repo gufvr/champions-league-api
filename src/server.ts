841232b1a22afbb5862091e982835a1bf567f6ea
GuFvr <gustavo.vfavero@gmail.com>
2024-09-25 22:28:16 -0300

    feat: set up Express server with TypeScript and basic route, added build and start scripts

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express, { json, Request, response, Response } from 'express';
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ player: 'Beckham' });
+});
+
+app.listen(port, () => {
+  console.log(`server running at port http://localhost/${port} 🎉`);
+});
